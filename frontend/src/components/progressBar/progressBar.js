e601006bc4e8c49937e4b7ba4c0b8504e64b09ac
Omprakash Choudhary <omprakash choudhary>
2024-03-23 16:58:14 +0530

    delaying loading

--- frontend/src/components/progressBar/progressBar.js
+++ frontend/src/components/progressBar/progressBar.js
@@ -10,7 +10,7 @@ export default function Progressbar(props) {
     let _val = value;
 
     interval.current = setInterval(() => {
-      _val += Math.floor(Math.random() * 7) + 1;
+      _val += Math.floor(Math.random() * 6) + 1;
 
       if (props.loading || _val >= 100) {
         _val = 100;
